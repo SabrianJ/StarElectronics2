07053fe1566dbbbb0766dba793519a923e3833a7
“Sabrian <sabrian.salim@gmail.com>
2022-05-09 19:32:07 +1200

    Allow all host

--- StarElectronics2/settings.py
+++ StarElectronics2/settings.py
@@ -13,8 +13,6 @@ import os
 from pathlib import Path
 import django_heroku
 
-django_heroku.settings(locals())
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +28,8 @@ SECRET_KEY = 'django-insecure-8j+i%^h08m==&c_uwi6$#rr=9n2&-2hj(4=q)+)he7n!lp=fey
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '0.0.0.0'
+    "*",
+    '0.0.0.0',
     'star-electronics.herokuapp.com',
     '127.0.0.1'
 ]
