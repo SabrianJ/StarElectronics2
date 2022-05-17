fa36cdd06ff8dd5c2605179025c3eced2af7834f
“Sabrian <sabrian.salim@gmail.com>
2022-05-17 20:42:44 +1200

    Add postgres

--- StarElectronics2/settings.py
+++ StarElectronics2/settings.py
@@ -80,12 +80,20 @@ WSGI_APPLICATION = 'StarElectronics2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgresql-cubed-19577',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -144,5 +152,8 @@ STATICFILES_FINDERS = [
 ]
 
 CURRENCY = '$'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 
